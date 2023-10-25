f9f9bf829fdf778d0bd64dad464e46a7aa0252fd
1khazipov <aaooo00aa0@gmail.com>
2023-10-25 23:41:22 +0300

    edited networks.py

--- networks.py
+++ networks.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import models
+import os
+from torch.nn.utils import spectral_norm
+import numpy as np
+
+import functools
 
 
 class ConditionGenerator(nn.Module):
@@ -307,6 +312,11 @@ y_vgg[i].detach())
         return loss
 
 
+# Defines the GAN loss which uses either LSGAN or the regular GAN.
+# When LSGAN is used, it is basically same as MSELoss,
+# but it abstracts away the need to create the target label tensor
+# that has the same size as the input
+
 class GANLoss(nn.Module):
     def __init__(self, use_lsgan=True, target_real_label=1.0, 
 target_fake_label=0.0,
@@ -358,4 +368,182 @@ target_is_real)
             target_tensor = self.get_target_tensor(input[-1], 
 target_is_real)
             return self.loss(input[-1], target_tensor)
+
+
+class MultiscaleDiscriminator(nn.Module):
+    def __init__(self, input_nc, ndf=64, n_layers=3, 
+norm_layer=nn.BatchNorm2d,
+                 use_sigmoid=False, num_D=3, getIntermFeat=False, 
+Ddownx2=False, Ddropout=False, spectral=False):
+        super(MultiscaleDiscriminator, self).__init__()
+        self.num_D = num_D
+        self.n_layers = n_layers
+        self.getIntermFeat = getIntermFeat
+        self.Ddownx2 = Ddownx2
+
+        for i in range(num_D):
+            netD = NLayerDiscriminator(input_nc, ndf, n_layers, 
+norm_layer, use_sigmoid, getIntermFeat, Ddropout,
+                                       spectral=spectral)
+            if getIntermFeat:
+                for j in range(n_layers + 2):
+                    setattr(self, 'scale' + str(i) + '_layer' + str(j), 
+getattr(netD, 'model' + str(j)))
+            else:
+                setattr(self, 'layer' + str(i), netD.model)
+
+        self.downsample = nn.AvgPool2d(3, stride=2, padding=[1, 1], 
+count_include_pad=False)
+
+    def singleD_forward(self, model, input):
+        if self.getIntermFeat:
+            result = [input]
+            for i in range(len(model)):
+                result.append(model[i](result[-1]))
+            return result[1:]
+        else:
+            return [model(input)]
+
+    def forward(self, input):
+        num_D = self.num_D
+
+        result = []
+        if self.Ddownx2:
+            input_downsampled = self.downsample(input)
+        else:
+            input_downsampled = input
+        for i in range(num_D):
+
+            if self.getIntermFeat:
+                model = [getattr(self, 'scale' + str(num_D - 1 - i) + 
+'_layer' + str(j)) for j in
+                         range(self.n_layers + 2)]
+            else:
+                model = getattr(self, 'layer' + str(num_D - 1 - i))
+            result.append(self.singleD_forward(model, input_downsampled))
+            if i != (num_D - 1):
+                input_downsampled = self.downsample(input_downsampled)
+        return result
+
+
+class NLayerDiscriminator(nn.Module):
+    def __init__(self, input_nc, ndf=64, n_layers=3, 
+norm_layer=nn.BatchNorm2d, use_sigmoid=False, getIntermFeat=False,
+                 Ddropout=False, spectral=False):
+        super(NLayerDiscriminator, self).__init__()
+        self.getIntermFeat = getIntermFeat
+        self.n_layers = n_layers
+        self.spectral_norm = spectral_norm if spectral else lambda x: x
+
+        kw = 4
+        padw = int(np.ceil((kw - 1.0) / 2))
+        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, 
+padding=padw), nn.LeakyReLU(0.2, True)]]
+
+        nf = ndf
+        for n in range(1, n_layers):
+            nf_prev = nf
+            nf = min(nf * 2, 512)
+            if Ddropout:
+                sequence += [[
+                    self.spectral_norm(nn.Conv2d(nf_prev, nf, 
+kernel_size=kw, stride=2, padding=padw)),
+                    norm_layer(nf), nn.LeakyReLU(0.2, True), 
+nn.Dropout(0.5)
+                ]]
+            else:
+
+                sequence += [[
+                    self.spectral_norm(nn.Conv2d(nf_prev, nf, 
+kernel_size=kw, stride=2, padding=padw)),
+                    norm_layer(nf), nn.LeakyReLU(0.2, True)
+                ]]
+
+        nf_prev = nf
+        nf = min(nf * 2, 512)
+        sequence += [[
+            nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=1, 
+padding=padw),
+            norm_layer(nf),
+            nn.LeakyReLU(0.2, True)
+        ]]
+
+        sequence += [[nn.Conv2d(nf, 1, kernel_size=kw, stride=1, 
+padding=padw)]]
+
+        if use_sigmoid:
+            sequence += [[nn.Sigmoid()]]
+
+        if getIntermFeat:
+            for n in range(len(sequence)):
+                setattr(self, 'model' + str(n), 
+nn.Sequential(*sequence[n]))
+        else:
+            sequence_stream = []
+            for n in range(len(sequence)):
+                sequence_stream += sequence[n]
+            self.model = nn.Sequential(*sequence_stream)
+
+    def forward(self, input):
+        if self.getIntermFeat:
+            res = [input]
+            for n in range(self.n_layers + 2):
+                model = getattr(self, 'model' + str(n))
+                res.append(model(res[-1]))
+            return res[1:]
+        else:
+            return self.model(input)
+
+
+def save_checkpoint(model, save_path, opt):
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path))
+
+    torch.save(model.cpu().state_dict(), save_path)
+    if opt.cuda:
+        model.cuda()
+
+
+def load_checkpoint(model, checkpoint_path, opt):
+    if not os.path.exists(checkpoint_path):
+        print('no checkpoint')
+        raise
+    log = model.load_state_dict(torch.load(checkpoint_path), strict=False)
+    if opt.cuda:
+        model.cuda()
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+def get_norm_layer(norm_type='instance'):
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False)
+    else:
+        raise NotImplementedError('normalization layer [%s] is not found' 
+% norm_type)
+    return norm_layer
+
+
+def define_D(input_nc, ndf=64, n_layers_D=3, norm='instance', 
+use_sigmoid=False, num_D=2, getIntermFeat=False,
+             gpu_ids=[], Ddownx2=False, Ddropout=False, spectral=False):
+    norm_layer = get_norm_layer(norm_type=norm)
+    netD = MultiscaleDiscriminator(input_nc, ndf, n_layers_D, norm_layer, 
+use_sigmoid, num_D, getIntermFeat, Ddownx2,
+                                   Ddropout, spectral=spectral)
+    print(netD)
+    if len(gpu_ids) > 0:
+        assert (torch.cuda.is_available())
+        netD.cuda()
+    netD.apply(weights_init)
+    return netD
 
