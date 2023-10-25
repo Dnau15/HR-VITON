f31760e20cfe714f41737c31728b1c7c472ab098
1khazipov <aaooo00aa0@gmail.com>
2023-10-25 19:55:27 +0300

    added utils.py

---
+++ utils.py
@@ -0,0 +1,25 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+import torch.nn.functional as F
+import numpy as np
+import cv2
+import os
+
+
+def cross_entropy2d(input, target, weight=None, size_average=True):
+    n, c, h, w = input.size()
+    nt, ht, wt = target.size()
+
+    # Handle inconsistent size between input and target
+    if h != ht or w != wt:
+        input = F.interpolate(input, size=(ht, wt), mode="bilinear", 
+align_corners=True)
+
+    input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    target = target.view(-1)
+    loss = F.cross_entropy(
+        input, target, weight=weight, size_average=size_average, 
+ignore_index=250
+    )
+    return loss
