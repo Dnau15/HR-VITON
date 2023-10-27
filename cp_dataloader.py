5146b28754a69b0cbf8ce068e95bd05607175f53
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2023-10-27 18:36:46 +0300

    Update cp_dataloader.py

--- cp_dataloader.py
+++ cp_dataloader.py
@@ -6,16 +6,13 @@ class CPDataLoader(object):
         super(CPDataLoader, self).__init__()
 
         if opt.shuffle :
-            train_sampler = 
-torch.utils.data.sampler.RandomSampler(dataset)
+            train_sampler = torch.utils.data.sampler.RandomSampler(dataset)
         else:
             train_sampler = None
 
         self.data_loader = torch.utils.data.DataLoader(
-                dataset, batch_size=opt.batch_size, shuffle=(train_sampler 
-is None),
-                num_workers=opt.workers, pin_memory=True, drop_last=True, 
-sampler=train_sampler)
+                dataset, batch_size=opt.batch_size, shuffle=(train_sampler is None),
+                num_workers=opt.workers, pin_memory=True, drop_last=True, sampler=train_sampler)
         self.dataset = dataset
         self.data_iter = self.data_loader.__iter__()
 
